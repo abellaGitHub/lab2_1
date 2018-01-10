cfa3abcde6a1cb58a01b726e3987d59e2847b8b7
smoczynskaj <smoczynska.justyna@gmail.com>
2018-01-10 20:18:47 +0100

    testy_v1 Testy elementów krańcowych sekwencji

--- src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
+++ src/test/java/edu/iis/mto/bsearch/BinarySearchTest.java
@@ -30,4 +30,26 @@ public class BinarySearchTest {
         assertFalse(result.isFound());
         assertEquals(result.getPosition(), -1);
     }
+
+    @Test
+    public void isFirstElement() throws Exception {
+
+        int[] seq = new int[]{2, 3, 5, 11, 13, 17};
+
+        SearchResult result = BinarySearch.search(2, seq);
+
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 1);
+    }
+
+    @Test
+    public void isLastElement() throws Exception {
+
+        int[] seq = new int[]{2, 3, 5, 11, 13, 17};
+
+        SearchResult result = BinarySearch.search(17, seq);
+
+        assertTrue(result.isFound());
+        assertEquals(result.getPosition(), 6);
+    }
 }
