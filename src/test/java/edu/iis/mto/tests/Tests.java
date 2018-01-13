8f6b806a97a3f171e52b30914270c326ae6f5c2e
Aleksander Bella <bella.aleksander@gmail.com>
2018-01-13 17:33:13 +0100

    testy_v1 Added test for searching one element which is in one element sequence

---
+++ src/test/java/edu/iis/mto/tests/Tests.java
@@ -0,0 +1,24 @@
+package edu.iis.mto.tests;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+import edu.iis.mto.bsearch.BinarySearch;
+import edu.iis.mto.bsearch.SearchResult;
+
+public class Tests {
+
+    @Test
+    public void elementSearchedIsInSequence() {
+        int key = 1;
+        int[] seq = {1};
+        int position = 1;
+
+        SearchResult res = BinarySearch.search(key, seq);
+
+        assertTrue(res.isFound());
+        assertEquals(res.getPosition(), position);
+    }
+}
