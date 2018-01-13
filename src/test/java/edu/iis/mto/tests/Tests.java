36fb197c1341d51ec3c09fdaeb88e73775789da3
Aleksander Bella <bella.aleksander@gmail.com>
2018-01-13 17:39:03 +0100

    testy_v1 Added test for searching one element which is not in one element sequence

--- src/test/java/edu/iis/mto/tests/Tests.java
+++ src/test/java/edu/iis/mto/tests/Tests.java
@@ -1,6 +1,7 @@
 package edu.iis.mto.tests;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
@@ -19,6 +20,18 @@ public class Tests {
         SearchResult res = BinarySearch.search(key, seq);
 
         assertTrue(res.isFound());
+        assertEquals(res.getPosition(), position);
+    }
+
+    @Test
+    public void elementSearchedIsNotInSequence() {
+        int key = 0;
+        int[] seq = {1};
+        int position = -1;
+
+        SearchResult res = BinarySearch.search(key, seq);
+
+        assertFalse(res.isFound());
         assertEquals(res.getPosition(), position);
     }
 }
